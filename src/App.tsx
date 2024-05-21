4240284d217a83f511ab69a961f408cc6f1a49d3
kabir Malviya <kabirmalviya@kabirs-Air.mshome.net>
2024-05-21 16:14:59 +0530

    ooo

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
 
   console.log(typingPosition);
   useEffect(() => {
-    const newSocket = io("https://code-compiler-backend-mu.vercel.app/", {
+    const newSocket = io("https://code-compiler-backend-mu.vercel.app", {
       transports: ["websocket"],
       withCredentials: true,
     });
